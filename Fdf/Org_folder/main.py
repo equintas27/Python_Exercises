5ff74b7f5a528a1ed01e7887db95791cbb8d4823
equintas27 <eugenioquintas20@gmail.com>
2026-06-22 16:36:18 +0100

    Finished Org_folder

--- Fdf/Org_folder/main.py
+++ Fdf/Org_folder/main.py
@@ -1,7 +1,7 @@
 import os
 import shutil 
 
-PASTA_ALVO = "../Testes"
+PASTA_ALVO = "./Testes"
 
 CATEGORIAS = {
     "Videos" : [".mp4", ".mkv"],
@@ -11,9 +11,23 @@ CATEGORIAS = {
 arquivos = os.listdir(PASTA_ALVO)
 
 for arquivo in arquivos:
+
+    pasta_origem = os.path.join(PASTA_ALVO, arquivo)
     nome, extensao = os.path.splitext(arquivo)
+    if os.path.isdir(pasta_origem):
+        continue 
+    pasta_destino = "Outros"
     for nome_pasta, extensao_pasta in CATEGORIAS.items():
         if extensao in extensao_pasta:
             pasta_destino = nome_pasta
             break 
-print(f"arquivo original: {nome}, extensão: {extensao}")
+    caminho_pasta_destino = os.path.join(PASTA_ALVO, pasta_destino)
+    if not os.path.isdir(caminho_pasta_destino):
+        os.makedirs(caminho_pasta_destino)
+        print(f"Nova pasta criada: {pasta_destino}")
+    caminho_final_arquivo = os.path.join(caminho_pasta_destino, arquivo)
+    shutil.move(pasta_origem, caminho_final_arquivo)
+    print(f"Movido: {arquivo} -> {pasta_destino}/")
+print ("Organização finalizada!")
+
+
