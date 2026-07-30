0ab0116df4b903062b1c2169c77fb355e0fbc76c
equintas27 <eugenioquintas20@gmail.com>
2026-07-30 19:54:47 +0100

    Adding new concepts of Python

---
+++ Fdf/Python/print.py
@@ -0,0 +1,30 @@
+print (3 * "Viva Angola")
+
+print ("Eu tenho ", 24 ,"de idade")
+
+b = "Angola"
+c = str(3)
+
+print(type(b))
+print (c * 3)
+
+x, y, z = "Banana", "Maçã", "Morango"
+
+print (x + y + z)
+
+print (x)
+print (y)
+print (z)
+
+a = b = c  = "Angola"
+
+print (a)
+print (b)
+print (c)
+
+fruits = ["Angola", "Argentina", "Espanha"]
+
+d = e = f = fruits
+print (d)
+print (e)
+print (f)
